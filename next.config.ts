972e35bb392b9614515ce486750373f2dd9f43a0
Sadeed <sadeed.javaid07@gmail.com>
2026-07-29 21:32:32 +0500

    change

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
